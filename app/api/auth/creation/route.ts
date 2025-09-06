6dcd769096a97901736efab7b8b4eff055a5f4b1
Семён Курек <sakashimaideal@gmail.com>
2025-09-06 10:36:38 +0800

    Changed redirects

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -29,5 +29,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/dashboard");
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`);
 }
--- app/dashboard/pricing/page.tsx
+++ app/dashboard/pricing/page.tsx
@@ -39,7 +39,7 @@ export default async function PricingPage() {
 
     const session = await stripe.billingPortal.sessions.create({
       customer: data?.User?.customerId as string,
-      return_url: "http://localhost:3000/dashboard",
+      return_url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
     });
 
     return redirect(session.url as string);
